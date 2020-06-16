838f13a9b19d4512cf0eb64e5420c21491ce081a
gk42111 <56386645+gk42111@users.noreply.github.com>
2020-06-16 16:42:48 +0200

    Add files via upload

--- Complex.cpp
+++ Complex.cpp
@@ -24,7 +24,7 @@ Complex operator*(const Complex & C1, const Complex & C2)
 {
 	Complex Wynik;
 	Wynik.Re = (C1.Re*C2.Re) - (C1.Im*C2.Im);
-	Wynik.Im = (C1.Re*C2.Im) - (C1.Im*C2.Re);
+	Wynik.Im = (C1.Re*C2.Im) + (C1.Im*C2.Re);
 	return Wynik;
 }
 
@@ -39,7 +39,7 @@ Complex operator-(const Complex &c1, const Complex &c2)
 Complex operator/(const Complex &c1, const Complex &c2)
 {
 	Complex Wynik;
-	Wynik.Re = ((c1.Re*c2.Re) - (c1.Im*c2.Im)) / (((pow(c2.Re, 2))) + ((pow(c2.Im, 2))));
+	Wynik.Re = ((c1.Re*c2.Re) + (c1.Im*c2.Im)) / (((pow(c2.Re, 2))) + ((pow(c2.Im, 2))));
 	Wynik.Im = ((c1.Re*c2.Im) - (c1.Im*c2.Re)) / (((pow(c2.Re, 2))) + ((pow(c2.Im, 2))));
 	return Wynik;
 }
